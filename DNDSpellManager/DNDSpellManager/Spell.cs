63bc6defa2f3f3c695feabc4769be88c33a556d8
Josh9309 <joshmalmquist96@gmail.com>
2015-07-11 21:29:12 -0400

    Add Spell and SpellDatabase Class
    finished basic for spell class and got the template for the spell
    database class

---
+++ DNDSpellManager/DNDSpellManager/Spell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDSpellManager
+{
+    class Spell
+    {
+        #region Attributes
+        //attributes
+        string spellName;
+        int lvl;
+        string type;
+        string duration;
+        int range;
+        int castTime;
+        string description;
+        #endregion 
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+        //constructor
+        public Spell(string name, int level, string spelltype, string spDuration, int spRange, int spCastTime, string descript)
+        {
+            spellName = name;
+            lvl = level;
+            type = spelltype;
+            duration = spDuration;
+            range = spRange;
+            castTime = spCastTime;
+            description = descript;
+        }
+        #endregion
+
+        #region Methods
+        public string GetInfo()
+        {
+            return description;
+        }
+        #endregion methods
+
+    }
+}
---
+++ DNDSpellManager/DNDSpellManager/SpellDatabase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNDSpellManager
+{
+    class SpellDatabase
+    {
+        #region Attributes
+        Dictionary<string, Spell> spellDict;
+        List<Spell> spellsList;
+        #endregion
+        
+        #region Constructor
+        public SpellDatabase()
+        {
+
+        }
+        #endregion
+
+        #region Methods
+        public void Import()
+        {
+
+        }
+
+        public void Export()
+        {
+
+        }
+
+        public List<string> GetAllSpellNames()
+        {
+            return null;
+        }
+        #endregion
+    }
+}
